7d450e6b61b91a6fe36409b5d72f3fb878b66855
Pieter Edelman <p.edelman@gmail.com>
2016-05-28 21:08:25 +0200

    Added the multimedia requirement to the project

--- transcribe.cpp
+++ transcribe.cpp
@@ -1,3 +1,5 @@
 #include "transcribe.h"
 
-Transcribe::Transcribe(int& argc, char** argv) : QGuiApplication(argc, argv) {}
+Transcribe::Transcribe(int& argc, char** argv) : QGuiApplication(argc, argv) {
+  m_player = new QMediaPlayer;
+}
--- transcribe.h
+++ transcribe.h
@@ -2,11 +2,17 @@
 #define TRANSCRIBE_H
 
 #include <QGuiApplication>
+#include <QMediaPlayer>
 
+/** The main application class. */
 class Transcribe : public QGuiApplication {
 
 public:
   Transcribe(int &argc, char **argv);
+
+private:
+  /** The main QMediaPlayer instance for playing and seeking audio files. */
+  QMediaPlayer* m_player;
 };
 
 #endif // TRANSCRIBE_H
